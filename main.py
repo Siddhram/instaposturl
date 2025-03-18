60e1d1402d7d19a10895545c3ff4288293425355
Siddharam <siddharamsutar23@gmail.com>
2025-03-18 08:17:53 +0530

    ok

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import instaloader
+
+app = Flask(__name__)
+CORS(app)
+
+loader = instaloader.Instaloader()
+
+def get_instagram_image(post_url):
+    try:
+        shortcode = post_url.split('/')[-2]
+        post = instaloader.Post.from_shortcode(loader.context, shortcode)
+        return {"image_url": post.url}
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.route('/get_image', methods=['POST'])
+def fetch_image():
+    data = request.get_json()
+    post_url = data.get("post_url")
+    if not post_url:
+        return jsonify({"error": "No post URL provided"}), 400
+    return jsonify(get_instagram_image(post_url))
+
+if __name__ == "__main__":
+    app.run(debug=True)
